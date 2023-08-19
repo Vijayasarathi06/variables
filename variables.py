97c99b9491dc135460ec7adb51d084b3fbb93478
Vijayasarathi06 <141705162+Vijayasarathi06@users.noreply.github.com>
2023-08-19 09:43:47 +0530

    variables

---
+++ variables.py
@@ -0,0 +1,12 @@
+name='vijayasarathi'
+Age =20
+Mobile=7339259341
+Email_id='gvijayasarathi286@gmail.com'
+Address='No.62 R.K Nagar,Ariyankuppam,puducherry'
+print('\tmy name is',name)
+print('\tmy Age is',Age)
+print('\tmy Mobile is',Mobile)
+print('\tmy email id is',Email_id)
+print('\tmy Address is',Address)
+
+
