358009e518ac1f6eeec075852557e68c8fb31bd5
Vijayasarathi06 <141705162+Vijayasarathi06@users.noreply.github.com>
2023-08-19 09:51:14 +0530

    Task 1

---
+++ task3.py
@@ -0,0 +1,9 @@
+##n=int(input("enter the value of  number of minutes"))
+##hr=n/60
+##print("hours:",hr)
+
+##F=float(input("enter the value of fahrenheit"))
+##c=(F-32)*5/9
+##print("Temperature of the celsius is:",c)
+
+print(input("enter the value of pencile"))
